c327a20d45c6d205aaa2cdf45dce71c571df66cb
eddie <eddie@lacoal.net>
2023-04-27 17:47:26 +0800

    start for LINE

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ openai.api_key = os.getenv("API_KEY")
 
 app = Flask(__name__, static_folder='static', template_folder='templates', static_url_path='/static')
 
+prev_message = ""
+prev_answer = ""
 
 @app.route('/')
 def index():
@@ -26,6 +28,7 @@ def chatbot():
         temperature=0.7,        # 越高越有創意
     )
 
+    print(response)
     for choice in response.choices:
         if "text" in choice:
             return choice.text
@@ -35,3 +38,19 @@ def chatbot():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+
+#HOW TO RUN
+# 1. create virtual environment: (At root dir)python3 -m venv venv
+# 2. activate virtual environment: source venv/bin/activate
+# 3. install packages: pip install -r requirements.txt
+# 4. run app: python3 app.py
+
+#HOW TO RUN WITH DOCKER
+# 1. build image: docker build -t chatgpt .
+# 2. run container: docker run -d -p 5000:5000 chatgpt
+# 3. check container: docker ps
+# 4. Open browser and go to http://localhost:5000/
+# 5. stop container: docker stop <container id>
+# 6. remove container: docker rm <container id>
