2d274c44c117534a01b8065d3bd1ab5b00ad8d63
pikxl <skywalker0823@hotmail.com>
2023-05-19 21:00:21 +0800

    fix except try issue

--- app.py
+++ app.py
@@ -36,48 +36,48 @@ def callback():
 def handle_message(event):
     message = event.message.text
     user_id = event.source.user_id
-    try:
-        if message == "clear" or message == "清除" or message == "清空" or message == "清除歷史" or message == "清空歷史":
-            conversation_history[user_id] = []
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text="已清除歷史訊息")
-            )
-        elif message == "get_list":
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=str(user_list_in_memory))
-            )
-        elif message == "get_history":
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=str(conversation_history))
-            )
-        else:
-                if user_id not in conversation_history:
-                    conversation_history[user_id] = []
-                conversation_history[user_id].append({"role": "user", "content": message})
-                message_log = conversation_history[user_id]
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",  # 使用的模型
-                    messages=message_log,   # 對話紀錄
-                    max_tokens=4097,        # token上限
-                    stop="exit",            # 結束對話的字串
-                    temperature=0.7,   
-                )
-                conversation_history[user_id].append({"role": "assistant", "content": response.choices[0].message.content})
-                line_bot_api.reply_message(
-                    event.reply_token,
-                    TextSendMessage(text=response.choices[0].message.content)
-                )
-    except Exception as e:
-        print(e)
-        # clear conversation history
+    # try:
+    if message == "clear" or message == "清除" or message == "清空" or message == "清除歷史" or message == "清空歷史":
         conversation_history[user_id] = []
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="Oops! 發生了一點問題，已為您清除歷史訊息，請重新嘗試")
+            TextSendMessage(text="已清除歷史訊息")
         )
+    elif message == "get_list":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=str(user_list_in_memory))
+        )
+    elif message == "get_history":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=str(conversation_history))
+        )
+    else:
+            if user_id not in conversation_history:
+                conversation_history[user_id] = []
+            conversation_history[user_id].append({"role": "user", "content": message})
+            message_log = conversation_history[user_id]
+            response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",  # 使用的模型
+                messages=message_log,   # 對話紀錄
+                max_tokens=4097,        # token上限
+                stop="exit",            # 結束對話的字串
+                temperature=0.7,   
+            )
+            conversation_history[user_id].append({"role": "assistant", "content": response.choices[0].message.content})
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=response.choices[0].message.content)
+            )
+    # except Exception as e:
+    #     print(e)
+    #     # clear conversation history
+    #     conversation_history[user_id] = []
+    #     line_bot_api.reply_message(
+    #         event.reply_token,
+    #         TextSendMessage(text="Oops! 發生了一點問題，已為您清除歷史訊息，請重新嘗試")
+    #     )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
